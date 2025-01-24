8a178bb0dd96116e62a2c610126cb6c0ff411d1b
Unai Hernández Minaberry <unihernandez22@gmail.com>
2025-01-24 17:50:39 +0100

    Fix force for jump

--- Assets/Scripts/CircleController.cs
+++ Assets/Scripts/CircleController.cs
@@ -5,11 +5,13 @@ public class CircleController : MonoBehaviour
 {
     int jumpForce = 5;
     InputAction jumpAction;
+    Rigidbody2D rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         jumpAction = InputSystem.actions.FindAction("Jump");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,7 @@ public class CircleController : MonoBehaviour
         if (jumpAction.WasPressedThisFrame())
         {
             Debug.Log("W key down");
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 }
