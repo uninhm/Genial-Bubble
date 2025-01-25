86c70a0e831a0e7c9bf908c633296ba7499a5801
SquiDecim <squidecim21@gmail.com>
2025-01-25 17:40:15 +0100

    New Spider

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 5f;
+    public float distance = 5f;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        float pingPong = Mathf.PingPong(Time.time * speed, distance);
+        transform.position = startPosition + Vector3.right * pingPong;
+    }
     public void takeShot()
     {
         Destroy(gameObject);
