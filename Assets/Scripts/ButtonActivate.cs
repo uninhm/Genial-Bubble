fcdc534359190ad36bf9340bab6f14a55b02528c
Unai Hernández Minaberry <unihernandez22@gmail.com>
2025-01-25 11:32:29 +0100

    Fix block gravity

--- Assets/Scripts/ButtonActivate.cs
+++ Assets/Scripts/ButtonActivate.cs
@@ -17,6 +17,7 @@ public class ButtonActivate : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        block.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
         block.GetComponent<Rigidbody2D>().gravityScale = 1;
         if (other.gameObject.CompareTag("GameController"))
         {
