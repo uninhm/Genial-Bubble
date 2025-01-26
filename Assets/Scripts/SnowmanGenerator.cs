e4cc8cf9bdee84111991446c9782bfe2bf19208e
SquiDecim <squidecim21@gmail.com>
2025-01-26 11:28:39 +0100

    Snowman Throw Trajectory

--- Assets/Scripts/SnowmanGenerator.cs
+++ Assets/Scripts/SnowmanGenerator.cs
@@ -6,14 +6,23 @@ public class SnowmanGenerator : Resetable
     public bool activated = false;
     public GameObject Snowman;
     public Transform SpawnPointSnowman;
+    private GameObject CurrentSnowman;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (!activated && collider.gameObject.name == "Player")
         {
-            activated = true;
-            Instantiate(Snowman, SpawnPointSnowman.position, transform.rotation);
+            SpawnSnowman();
         }
+    }
+    private void SpawnSnowman()
+    {
+        if (CurrentSnowman != null)
+        {
+            Destroy(CurrentSnowman);
+        }
+        activated = true;
+        CurrentSnowman = Instantiate(Snowman, SpawnPointSnowman.position, transform.rotation);
     }
 
     override public void Reset()
--- Assets/Scripts/SnowmanThrow.cs
+++ Assets/Scripts/SnowmanThrow.cs
@@ -2,5 +2,31 @@ using UnityEngine;
 
 public class SnowmanThrow : MonoBehaviour
 {
-    
+    public GameObject Snowball;
+    public float launchForce = 6f;
+    public float launchAngle = 90f;
+    public float fireRate = 0.5f; // Tirs par seconde
+    private float nextFireTime = 0f;
+
+    void Update()
+    {
+        if (Time.time >= nextFireTime)
+        {
+            LaunchBall();
+            nextFireTime = Time.time + 1f / fireRate;
+        }
+
+    }
+    void LaunchBall()
+    {
+        GameObject ball = Instantiate(Snowball, transform.position, Quaternion.identity);
+        Rigidbody2D rb = ball.GetComponent<Rigidbody2D>();
+
+        // Calculer la direction toujours vers la gauche
+        Vector2 launchDirection = new Vector2(-Mathf.Cos(launchAngle * Mathf.Deg2Rad), Mathf.Sin(launchAngle * Mathf.Deg2Rad));
+
+        rb.AddForce(launchDirection * launchForce, ForceMode2D.Impulse);
+
+        Destroy(ball, 5f);
+    }
 }
